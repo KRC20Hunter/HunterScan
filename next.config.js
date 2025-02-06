2aeb7b05dbdef5f5256aa1a1103a9635d9a53dba
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2025-02-06 12:39:32 +0200

    d

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    output: 'export',
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//     output: 'export',
    
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
+//     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+//     // trailingSlash: true,
    
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
+//     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+//     // skipTrailingSlashRedirect: true,
    
-    // Optional: Change the output directory `out` -> `dist`
-    distDir: 'dist',
-    images: {unoptimized: true,},
-  }
+//     // Optional: Change the output directory `out` -> `dist`
+//     distDir: 'dist',
+//     images: {unoptimized: true,},
+//   }
    
-  module.exports = nextConfig
+//   module.exports = nextConfig
