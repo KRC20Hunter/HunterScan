7437512674c5b00d22da86cccf6f56131bbd120a
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2025-02-06 13:51:40 +0200

    new

--- next.config.js
+++ next.config.js
@@ -1,20 +1,34 @@
 /**
  * @type {import('next').NextConfig}
  */
-const withTM = require('next-transpile-modules')(['nacho-component-library']);
-const nextConfig = {
-    output: 'export',
+// const withTM = require('next-transpile-modules')(['nacho-component-library']);
+// const nextConfig = {
+//     output: 'export',
    
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
+//     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+//     // trailingSlash: true,
    
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
+//     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+//     // skipTrailingSlashRedirect: true,
    
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-    // images: {unoptimized: true,},
+//     // Optional: Change the output directory `out` -> `dist`
+//     // distDir: 'dist',
+//     // images: {unoptimized: true,},
 
-  }
+//   }
 
-  module.exports = nextConfig
+
+  const withTM = require('next-transpile-modules')(['nacho-component-library']);
+
+    const nextConfig = withTM({
+        output: 'export',
+        webpack: (config) => {
+            config.module.rules.push({
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            });
+            return config;
+        },
+    });
+
+    module.exports = nextConfig;
