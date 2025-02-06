3221c5cadeeef73572b366c694fa3af4245008d7
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2025-02-06 14:18:39 +0200

    new

--- next.config.js
+++ next.config.js
@@ -22,6 +22,8 @@
     /** @type {import('next').NextConfig} */
     const nextConfig = {
         output: 'export',
+        distDir: 'dist',
+        images: {unoptimized: true,},
         // Add other configurations if necessary.
     };
 
